429c19a2b13f4bfd2ee72581673f13db561f2c12
amaliye <amaliesanchez@outlook.com>
2025-01-14 16:34:37 +0100

    la til app i root

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-//import app from "./app";
+import app from "./app";
 
-createRoot(document.getElementById("root")).render(<h1>Hello</h1>);
+createRoot(document.getElementById("root")).render(<app/>);
