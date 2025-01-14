ede3e6ba9fef765ebab74e3c7af2dc3d57f9a139
amaliye <amaliesanchez@outlook.com>
2025-01-14 16:36:20 +0100

    la til app i root, med prettier?

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import app from "./app";
 
-createRoot(document.getElementById("root")).render(<app/>);
+createRoot(document.getElementById("root")).render(<app />);
